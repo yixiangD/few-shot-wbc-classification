d4357a45439b0271b8426f6ea803a245383fff7c
yixiangdeng <doggydeng@gmail.com>
2020-09-13 20:44:12 -0400

    change tabs to spaces

--- src/utils.py
+++ src/utils.py
@@ -14,34 +14,34 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator,\
 from keras.utils.np_utils import to_categorical
 
 def get_data(folder):
-	"""
+    """
 	Load the data and labels from the given folder.
-	"""
-	X = []
-	y = []
-	for wbc_type in os.listdir(folder):
-		if not wbc_type.startswith('.'):
-			if wbc_type in ['NEUTROPHIL']:
-				label = 0
-			elif wbc_type in ['EOSINOPHIL']:
-				label = 1
-			elif wbc_type in ['MONOCYTE']:
-				label = 2
-			elif wbc_type in ['LYMPHOCYTE']:
-				label = 3
-			else:
-				label=4
-			for image_filename in tqdm(os.listdir(folder + wbc_type)):
-				img_file = cv2.imread(folder + wbc_type + '/' + image_filename)
-				if img_file is not None:
-					img_file = np.array(Image.fromarray(img_file).resize((80, 60)))
-					img_arr = np.asarray(img_file)
-					X.append(img_arr)
-					y.append(label)
-	X = np.asarray(X)
-	y = np.asarray(y)
-	y_Hot = to_categorical(y , num_classes = 5)
-	return X,y_Hot
+    """
+    X = []
+    y = []
+    for wbc_type in os.listdir(folder):
+        if not wbc_type.startswith('.'):
+            if wbc_type in ['NEUTROPHIL']:
+                label = 0
+            elif wbc_type in ['EOSINOPHIL']:
+                label = 1
+            elif wbc_type in ['MONOCYTE']:
+                label = 2
+            elif wbc_type in ['LYMPHOCYTE']:
+                label = 3
+            else:
+                label = 4
+            for image_filename in tqdm(os.listdir(folder + wbc_type)):
+                img_file = cv2.imread(folder + wbc_type + '/' + image_filename)
+                if img_file is not None:
+                    img_file = np.array(Image.fromarray(img_file).resize((80, 60)))
+                    img_arr = np.asarray(img_file)
+                    X.append(img_arr)
+                    y.append(label)
+    X = np.array(X)
+    y = np.array(y)
+    y_Hot = to_categorical(y, num_classes = 5)
+    return X, y_Hot
 
 def get_annotation(image, tree):
     for elem in tree.iter():
