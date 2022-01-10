37f17e085dfcf36a3c25473571abf30167661f7c
yixiangD <doggydeng@gmail.com>
2022-01-10 15:37:22 -0500

    update ignore file and hyper

--- src/hyper.py
+++ src/hyper.py
@@ -1,8 +1,8 @@
 BATCH_SIZE = 32
 LEARNING_RATE = 1e-4
-PATH1 = "../data/raw2"
-PATH2 = "../data/masked"
-IMG_SIZE = (160, 160)
+PATH1 = "./data/raw2"
+PATH2 = "./data/masked"
+IMG_SIZE = (480, 640)
 IMG_SHAPE = IMG_SIZE + (3,)
 initial_epochs = 10
 fine_tune_epochs = 40
