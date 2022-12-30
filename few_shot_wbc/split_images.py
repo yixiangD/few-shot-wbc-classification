81b76c9d10d8c623ee7747f5afab058f155fa3d5
yixiangD <doggydeng@gmail.com>
2022-12-29 22:08:57 -0500

    add split

--- few_shot_wbc/split_images.py
+++ few_shot_wbc/split_images.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import shutil
 from collections import Counter
 
 import numpy as np
@@ -32,10 +33,6 @@ def main():
     args = parser.parse_args()
     # print(args)
 
-    for root, dirs, files in os.walk(path):
-        root, dirs, files = root, dirs, files
-        break
-
     # reading table of labels and image names
     clean_path = "./data/labels_clean.csv"
     df_clean = pd.read_csv(clean_path, usecols=[1, 2])
@@ -54,24 +51,64 @@ def main():
     if args.crop:
         fold_name += "_crop"
 
-    # train_path = os.path.join("./data", fold_name, "train")
-    # test_path = os.path.join("./data", fold_name, "test")
+    if args.num_class == 2:
+        lab = "Lympho"
+        prefix = "crop"
+    else:
+        lab = "Category"
+        prefix = "BloodImage"
+        # exclude basophil
+        df = df[df["Category"] != "BASOPHIL"]
+        # print(Counter(df["Category"]))
+    out_path = os.path.join("./data", fold_name)
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
+
+    train_path = os.path.join(out_path, "train")
+    test_path = os.path.join(out_path, "test")
+
+    if not os.path.exists(train_path):
+        os.makedirs(train_path)
+    else:
+        # clear path before copying images
+        print("Deleting old train folder...")
+        shutil.rmtree(train_path)
+        os.makedirs(train_path)
+
+    if not os.path.exists(test_path):
+        os.makedirs(test_path)
+    else:
+        # clear path before copying images
+        print("Deleting old train folder...")
+        shutil.rmtree(test_path)
+        os.makedirs(test_path)
+
+    for root, dirs, files in os.walk(path):
+        root, dirs, files = root, dirs, files
+        break
 
     sampler = StratifiedShuffleSplit(
         n_splits=1, test_size=1 / float(args.nfold), random_state=0
     )
-    if args.num_class == 2:
-        lab = "Lympho"
-    else:
-        lab = "Category"
-        df = df[df["Category"] != "BASOPHIL"]
-        print(Counter(df["Category"]))
     for i, (train_id, test_id) in enumerate(
         sampler.split(df["Image"].values, df[lab].values)
     ):
-        print(f"fold {i}")
         print(f"Train: {train_id}")
         print(f"Test: {test_id}")
+        print("Sanity check...")
+        print(Counter(df.iloc[train_id, 2]))
+        print(Counter(df.iloc[test_id, 2]))
+        print(df.iloc[train_id, :])
+        x_train = df.iloc[train_id, 0].values
+        x_test = df.iloc[test_id, 0].values
+        print(x_train)
+        for index in x_train:
+            print(index)
+            fname = prefix + "_" + str(index).zfill(5) + ".jpg"
+            shutil.copy(os.path.join(path, fname), os.path.join(train_path, fname))
+        for index in x_test:
+            fname = prefix + "_" + str(index).zfill(5) + ".jpg"
+            shutil.copy(os.path.join(path, fname), os.path.join(test_path, fname))
 
 
 if __name__ == "__main__":
